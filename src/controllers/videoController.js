f49f1e2cbc88833bbcd41bdc674452c2cb6aa49e
CGJI58 <leemsk22@naver.com>
2022-05-05 15:55:25 +0900

    Set routers

---
+++ src/controllers/videoController.js
@@ -0,0 +1,8 @@
+export const home = async (req, res) => res.send("home");
+export const search = async (req, res) => res.send("search");
+export const watch = async (req, res) => res.send("watch");
+export const getUpload = async (req, res) => res.send("getUpload");
+export const postUpload = async (req, res) => res.send("postUpload");
+export const getEdit = async (req, res) => res.send("getEdit");
+export const postEdit = async (req, res) => res.send("postEdit");
+export const deleteVideo = async (req, res) => res.send("deleteVideo");
