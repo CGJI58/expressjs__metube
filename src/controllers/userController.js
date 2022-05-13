cec3d1c35128fa48c1e9c3f478faa2e7542706ef
CGJI58 <leemsk22@naver.com>
2022-05-13 15:26:30 +0900

    #7.6까지 진행함

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,65 @@
-export const join = (req, res) => res.send("Join");
+import User from "../models/User";
+import bcrypt from "bcrypt";
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+
+export const postJoin = async (req, res) => {
+  const { email, username, password, confirmPassword, name, location } =
+    req.body;
+  const pageTitle = "Join";
+  if (password !== confirmPassword) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "Password confirmation does not match.",
+    });
+  }
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    const checkExistItem = await User.exists({ email });
+    let existItem = "existItem";
+    if (checkExistItem) existItem = "email";
+    else existItem = "username";
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: `The ${existItem} is already taken.`,
+    });
+  }
+  await User.create({
+    email,
+    username,
+    password,
+    name,
+    location,
+  });
+  return res.redirect("/login");
+};
+
+export const getLogin = (req, res) => {
+  res.render("login", { pageTitle: "Login" });
+};
+
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const pageTitle = "Login";
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "An account with this username does not exists.",
+    });
+  }
+  const ok = bcrypt.compare(password, user.password);
+  if (!ok) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "Wrong password.",
+    });
+  }
+  console.log("Logged user in.");
+  res.redirect("/");
+};
+
 export const edit = (req, res) => res.render("edit");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("Login");
 export const logout = (req, res) => res.send("Log out");
 export const see = (req, res) => res.send("See User");
